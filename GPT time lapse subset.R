30a7e20d82e5bdb91625fb2d777070c82dcd50a9
klsyme <8371178+klsyme@users.noreply.github.com>
2025-09-14 14:40:04 +0100

    Update GPT time lapse subset.R

--- GPT time lapse subset.R
+++ GPT time lapse subset.R
@@ -6,7 +6,7 @@ library(irrCAC)
 
 ## This creates a data frame where each row has one paragraph
 paragraphs <- 
-  read_excel("Subset Codes Fasting rename.xlsx") |> 
+  read_excel("Datasets/Subset Codes Fasting rename.xlsx") |> 
   mutate(
     Excerpt.x = str_remove_all(Excerpt.x, "\\{\\{\\d+\\}\\}"),    
     Excerpt.x = str_remove_all(Excerpt.x, "[^\x1F-\x7F]+")      
