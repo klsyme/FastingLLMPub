dbd531287b907eff5333ef97cbf054d62e303d63
klsyme <8371178+klsyme@users.noreply.github.com>
2025-09-14 14:41:30 +0100

    Update GPT vk subset.R

--- GPT vk subset.R
+++ GPT vk subset.R
@@ -6,7 +6,7 @@ library(irrCAC)
 
 ## This creates a data frame where each row has one paragraph
 paragraphs <- 
-  read_excel("Cognitive Subset Unlisted.xlsx") |> 
+  read_excel("Datasets/Cognitive Subset Unlisted.xlsx") |> 
   mutate(
     Excerpt.x = str_remove_all(Excerpt.x, "\\{\\{\\d+\\}\\}"),    
     Excerpt.x = str_remove_all(Excerpt.x, "[^\x1F-\x7F]+")      
