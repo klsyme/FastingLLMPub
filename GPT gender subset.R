673182ddb0ef4d7509af8d97c0ad8ae23cc9a624
klsyme <8371178+klsyme@users.noreply.github.com>
2025-09-14 14:34:09 +0100

    Update GPT gender subset.R

--- GPT gender subset.R
+++ GPT gender subset.R
@@ -2,7 +2,7 @@ source("GPT Run.R")
 
 ## This creates a data frame where each row has one paragraph
 paragraphs <- 
-  read_excel("Subset Codes Fasting rename.xlsx") |> 
+  read_excel("Datasets/Subset Codes Fasting rename.xlsx") |> 
   mutate(
     Excerpt.x = str_remove_all(Excerpt.x, "[{][{][\\d ]+[}][}]"),
     Excerpt.x = str_remove_all(Excerpt.x, "[^\x1F-\x7F]+")
